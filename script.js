6495a24c0dc276bcd86cc8701ab54f9e5498c72b
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2022-03-01 02:09:07 +0300

    Вторая сдача проектной работы №4

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 // Открытие, закрытие попапа
 let popup = document.querySelector('.popup');
-let EditButton = document.querySelector('.ProfileInfo__EditButton');
-let closeButton = document.querySelector('.popup__close');
+const editButton = document.querySelector('.profile__editButton');
+const closeButton = document.querySelector('.popup__close');
+
+const like = document.querySelector('.element__like');
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__field_type_name');
+let jobInput = formElement.querySelector('.popup__field_type_description');
+
+let popupName = document.querySelector('.profile__name');
+let popupJob = document.querySelector('.profile__description');
+
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
 
-EditButton.addEventListener('click', togglePopup); 
+function handleOpenProfilePopup() {
+  togglePopup();
+  nameInput.value = popupName.textContent;
+  jobInput.value = popupJob.textContent;
+}
+
+editButton.addEventListener('click', handleOpenProfilePopup); 
 closeButton.addEventListener('click', togglePopup); 
 
 
 // Ставим лукасы
-let like = document.querySelector('.Element__like');
-
 function toggleLike() {
-  like.classList.toggle('Element__like_active');
+  like.classList.toggle('element__like_active');
 }
 
 like.addEventListener('click', toggleLike); 
 
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__Field_type_Name');
-let jobInput = formElement.querySelector('.popup__Field_type_Description');
-
-let popupName = document.querySelector('.ProfileInfo__Name');
-let popupJob = document.querySelector('.ProfileInfo__Description');
-
-nameInput.value = popupName.textContent;
-jobInput.value = popupJob.textContent;
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -40,16 +43,13 @@ function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                               // Так мы можем определить свою логику отправки.
                                               // О том, как это делать, расскажем позже.
-
+  togglePopup();
   
   popupName.textContent = nameInput.value;
   popupJob.textContent = jobInput.value;
-
-  popup.classList.toggle('popup_opened');
 
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-
+formElement.addEventListener('submit', formSubmitHandler);
