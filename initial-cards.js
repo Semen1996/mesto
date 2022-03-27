41ca1659229b600c24ad970d45a786525d380f9d
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2022-03-27 16:47:36 +0300

    Первая сдача проектной работы №6

---
+++ initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Карачаевск',
+      link: 'images/Karachaevsk.jpg',
+    },
+    {
+      name: 'Гора Эльбрус',
+      link: 'images/Elbrus.png',
+    },
+    {
+      name: 'Домбай',
+      link: 'images/Dombay.jpg',
+    },
+    {
+      name: 'Гора Эльбрус',
+      link: 'images/Elbrus.png',
+    },
+    {
+      name: 'Домбай',
+      link: 'images/Dombay.jpg',
+    },
+    {
+      name: 'Карачаево-Черкесия',
+      link: 'images/Karachaevsk.jpg',
+    }
+  ];
